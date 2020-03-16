5f7213674dea97e1fd4b49d780bf8b432f04f5cc
Lauryn Bassett <lauryn.bassett@gmail.com>
2020-03-16 13:21:30 -0500

    added marker.js and style from FS solution

--- src/marker.js
+++ src/marker.js
@@ -1,20 +1,20 @@
-const mapboxgl = require("mapbox-gl");
+const { Marker } = require('mapbox-gl');
 
 const markerURLs = {
-    activity: 'http://i.imgur.com/WbMOfMl.png',
-    hotel: 'http://i.imgur.com/D9574Cu.png',
-    restaurant: 'http://i.imgur.com/cqR6pUI.png'
-}
+	activity: 'https://i.imgur.com/WbMOfMl.png',
+	hotel: 'https://i.imgur.com/D9574Cu.png',
+	restaurant: 'https://i.imgur.com/cqR6pUI.png'
+};
 
-const makeMarker = function(coord, type) {
-    var el = document.createElement('div');
-    el.className = 'marker';
-    el.style.backgroundImage = `url(${markerURLs.type})`
-    return new mapboxgl.Marker(el).setLngLat(coord);
-    
-    }
+const buildMarker = function(type, coords) {
+	if (!markerURLs.hasOwnProperty(type)) type = 'activity';
+	let el = document.createElement('div');
+	el.style.backgroundSize = 'contain';
+	el.style.width = '32px';
+	el.style.height = '37px';
+	// el.className = 'marker';
+	el.style.backgroundImage = `url(${markerURLs[type]})`;
+	return new Marker(el).setLngLat(coords);
+};
 
-module.exports = makeMarker
-
-
-
+module.exports = buildMarker;
